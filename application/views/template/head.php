ecbc959336bbd31cb6420f701b35016c971b2563
dpapanikolaou <dionysis_172@hotmail.com>
2017-08-17 01:56:06 +0300

    - autoload url, form helpers - set base url - set database connection - insert template files

---
+++ application/views/template/head.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Author: Dionisis Papanikolaou
+ * Date: 16/8/2017
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<meta name="description" content="">
+<meta name="author" content="">
+<title>SB Admin - Start Bootstrap Template</title>
+
+<link href="<?php echo base_url(); ?>assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="<?php echo base_url(); ?>assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link href="<?php echo base_url(); ?>assets/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+<link href="<?php echo base_url(); ?>assets/css/sb-admin.css" rel="stylesheet">
